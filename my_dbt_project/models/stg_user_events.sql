61c4ae332fbf8e006d56d4e6d2000e2f10edb139
Datu1213 <zhenkuiwang1213@gmail.com>
2025-11-16 16:52:16 -0800

    Add S3 Support and refine sql model

--- my_dbt_project/models/stg_user_events.sql
+++ my_dbt_project/models/stg_user_events.sql
@@ -1,9 +1,18 @@
 -- models/stg_user_events.sql
 
+-- Hive will not genarate table
+-- It will store the view definition in the metastore
+-- that points to the data location
+-- which means there is no data duplication
+
+-- 'even_minute' is not user in dbt logic layer
+-- it will be used in physical layer(Spark) 
+-- for partitioning optimization
+
 select
     user_id,
     event_type,
     page,
     value as purchase_value,
     event_timestamp as ts -- Rename for clarity
-from delta.`/opt/spark/data/user_events_delta` -- <-- 重要：替换为 Spark Thrift Server 可以访问到的路径！
+from delta.`s3a://prod-data/data/user_events_delta`
